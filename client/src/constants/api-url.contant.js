f545f80abec572da7d47cca57e68bf164eb664d3
Leandro Piazza <lean-developer@proton.me>
2024-11-15 17:02:09 -0300

    Feat: Cambios para producción protocolo HTTPS

--- client/src/constants/api-url.contant.js
+++ client/src/constants/api-url.contant.js
@@ -1,2 +1,3 @@
+// export const API_URL = 'http://149.50.141.188:3000/api';
 // export const API_URL = 'http://localhost:3000/api';
-export const API_URL = 'http://149.50.141.188:3000/api';
+export const API_URL = 'https://naturalcycle.com.ar/api';
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Esto hace que el servidor escuche en todas las interfaces de red
+    port: 8000, // Puedes cambiar el puerto si ya estÃ¡ en uso
+  },
 });
