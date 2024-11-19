629417f407e5070656de5b501f176ec8b0da38cd
Alejo Serra <alejo.serra@wearesoftwares.com>
2024-11-19 15:40:26 -0300

    arreglo redirección al prod del banner y cambios estaticos en el footer

--- client/src/Components/SocialLinks.jsx
+++ client/src/Components/SocialLinks.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SocialLinks = () => {
   return (
     <div className="section-footer">
-      <h3>Ayuda</h3>
+      <h3>Redes</h3>
       <a target='blank' href="https://www.instagram.com/naturalcycle21?igsh=MXBzeWhyNHcxenVuNg==">
         <i className="fa-brands fa-instagram"></i> Instagram
       </a>
